c4bf237cdb88917e39b0c5ffe0cb0a1a8b40bc72
libbyleonard22 <815989@mystma.org>
2021-10-06 15:02:30 -0500

    3

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ def countA(w):
 
     w = 1
 
-    for i in range(1, len(w)):
+    for i in range(w, len(w)):
         total = total * i
     
-    if w[i] == "a":
+    if w[i] == "anteater":
         return a
 
 
