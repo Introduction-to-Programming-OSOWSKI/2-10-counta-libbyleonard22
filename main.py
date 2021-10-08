09aa71627f4044819f69136ff645589b048889e6
libbyleonard22 <815989@mystma.org>
2021-10-08 14:54:47 -0500

    4

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
 
 def countA(w):
-
     total = 0
-
     for i in range(0, len(w)):
         if w[i] == "a":
-            return "a"
+            total = total + 1
+    return total
 
-print(countA("armadillo"))
+countA("armadillo")
